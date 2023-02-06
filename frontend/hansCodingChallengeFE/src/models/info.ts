d87454154ce7123926005529fd0c1c6eaf6c55e2
luis-0104 <schweinberger@milongroup.com>
2023-02-06 15:01:46 +0100

    setInfo reset afterTimeout (geht ned schöner)

--- frontend/hansCodingChallengeFE/src/models/info.ts
+++ frontend/hansCodingChallengeFE/src/models/info.ts
@@ -1,9 +1,9 @@
-import { getParent, types } from "mobx-state-tree";
+import { flow, getParent, types } from "mobx-state-tree";
 import { Customers } from "./customers";
 export type infoType = {
   title: string;
   message: string;
-  type: "error" | "warning" | "info" | "success" ;
+  type: "error" | "warning" | "info" | "success";
 };
 export const Info = types
   .model({
@@ -12,18 +12,32 @@ export const Info = types
     type: types.maybe(
       types.enumeration(["error", "warning", "info", "success", "loading"])
     ),
-    loading: types.optional(types.boolean,false)
+    loading: types.optional(types.boolean, false),
   })
   .actions((self) => ({
     clear() {
       (self.title = ""), (self.message = ""), (self.type = undefined);
     },
-    setInformation({ title, message, type }: infoType) {
+    setInformation: flow(function* ({ title, message, type }: infoType) {
       (self.title = title), (self.message = message), (self.type = type);
-    },
-    setLoading(isLoading: boolean){
-      
-      self.loading = isLoading;
-    }
-  }));
+      yield new Promise((res) => setTimeout(res, 10000));
 
+      self.title = "";
+      self.message = "";
+      self.type = undefined;
+    }),
+    setLoading(isLoading: boolean) {
+      self.loading = isLoading;
+    },
+  }))
+  .actions((self) => ({
+    afterCreate() {
+      // After the page changes, we call the setInfo again, so it
+
+      self.setInformation({
+        title: self.title,
+        message: self.message,
+        type: self.type,
+      } as infoType);
+    },
+  }));
