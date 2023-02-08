6b962708491f9a3773a38db679359b03849ffe85
luis-0104 <schweinberger@milongroup.com>
2023-02-08 13:09:48 +0100

    no more sql inject

--- backend/mySQL/mySQLHandler.js
+++ backend/mySQL/mySQLHandler.js
@@ -15,20 +15,21 @@ con.connect(function (err) {
 function getAllCustomers(callback) {
   con.query("SELECT * from MOCK_DATA", (err, result, fields) => {
     if (err) {
-        console.error(`Error: ${err.message}`);
-      }
-      callback(err,result)
+      console.error(`Error: ${err.message}`);
+    }
+    callback(err, result);
   });
 }
 
 function getCustomerWithID(id, callback) {
   con.query(
-    `SELECT * from MOCK_DATA WHERE id = ${id}`,
+    "SELECT * from MOCK_DATA WHERE id = ?",
+    [id],
     (err, result, fields) => {
-        if (err) {
-            console.error(`Error: ${err.message}`);
-          }
-          callback(err,result[0])
+      if (err) {
+        console.error(`Error: ${err.message}`);
+      }
+      callback(err, result[0]);
     }
   );
 }
@@ -45,30 +46,31 @@ function createNewCustomer(customer, callback) {
 		password,
 		last_login
 	)
-values (
-		${customer.id},
-		'${customer.user_name}',
-		'${customer.first_name}',
-		'${customer.last_name}',
-		'${customer.birth_date}',
-		'${customer.email}',
-		'${customer.password}',
-		'${customer.last_login}'
-	);`,
+values (?,?,?,?,?,?,?,?);`,
+    [
+      customer.id,
+      customer.user_name,
+      customer.first_name,
+      customer.last_name,
+      customer.birth_date,
+      customer.email,
+      customer.password,
+      customer.last_login,
+    ],
     (err, result) => {
       if (err) {
         console.error(`Error: ${err.message}`);
       }
-      callback(err,result)
+      callback(err, result);
     }
   );
 }
 
 function deleteCustomerWithID(id, callback) {
-  con.query(`DELETE FROM MOCK_DATA WHERE id = ${id};`, (err, result) => {
+  con.query(`DELETE FROM MOCK_DATA WHERE id = ?;`, [id], (err, result) => {
     if (err) {
-        console.error(`Error: ${err.message}`);
-      }
+      console.error(`Error: ${err.message}`);
+    }
     callback(err, result.affectedRows == 1); //if its higher: more than 1 was deleted (not possible except sql injection) lower: row to delete didn't exist ->404
   });
 }
@@ -76,54 +78,38 @@ function deleteCustomerWithID(id, callback) {
 function updateCustomer(id, customer, callback) {
   con.query(
     `UPDATE MOCK_DATA SET
-            id = ${customer.id},
-            user_name = '${customer.user_name}',
-            first_name ='${customer.first_name}',
-            last_name = '${customer.last_name}',
-            birth_date = '${customer.birth_date}',
-            email = '${customer.email}',
-            password = '${customer.password}',
-            last_login = '${customer.last_login}'
-        WHERE id=${id}
+            id = ?,
+            user_name = ?, 
+            first_name = ?,
+            last_name = ?,
+            birth_date = ?,
+            email = ?,
+            password = ?, 
+            last_login = ?
+        WHERE id = ?
         ;`,
+    [
+      customer.id,
+      customer.user_name,
+      customer.first_name,
+      customer.last_name,
+      customer.birth_date,
+      customer.email,
+      customer.password,
+      customer.last_login,
+      customer.id,
+    ],
     (err, result) => {
-        if (err) {
-            console.error(`Error: ${err.message}`);
-          }
-          callback(err,result?(result.affectedRows == 1 ? customer : undefined):undefined)     
+      if (err) {
+        console.error(`Error: ${err.message}`);
+      }
+      callback(
+        err,
+        result ? (result.affectedRows == 1 ? customer : undefined) : undefined
+      );
     }
   );
 }
-
-// getAllCustomers((result) => {
-//   console.log(result[4]);
-// });
-// getCustomerWithID(12345, (result) => {
-//   console.log(result);
-// });
-
-// let c = {
-//   id: 12345,
-//   user_name: "BrotBernd123",
-//   first_name: "Bernd",
-//   last_name: "DasBaguette",
-//   birth_date: "2022-6-27",
-//   email: "Bernd@brot.de",
-//   password: "IchMag2Brot!",
-//   last_login: "2022-05-27 10:07:18",
-// };
-
-// deleteCustomerWithID(12345, (result) => {
-//   console.log(result);
-// });
-// createNewCustomer(c, (result) => {
-//   console.log(result);
-// });
-
-// c.last_name = "DasBrot"
-// updateCustomer(12345,c,(result)=>{
-//     console.log(result)
-// })
 
 exports.getAllCustomers = getAllCustomers;
 exports.getCustomerWithID = getCustomerWithID;
